d09511d31bc964706cc672f697fe4c1e88f32720
Kaefa Kanneh <kaefakanneh@hotmail.com>
2023-11-08 14:44:02 -0500

    Add Stepable Module

---
+++ practice-for-ch-ruby-chess-long-practice-main/pieces/stepable.rb
@@ -0,0 +1,17 @@
+module Stepable
+
+  def moves
+    move_diffs.map do |move_diff| 
+      pos.zip(move_diff).map(&:sum) 
+    end.select do |step_pos| 
+      board.valid_pos?(step_pos) 
+    end.reject do |step_pos|
+      board[step_pos].color == color 
+    end
+  end
+
+  private
+  def move_diffs
+    raise NotImplementedError
+  end
+end
